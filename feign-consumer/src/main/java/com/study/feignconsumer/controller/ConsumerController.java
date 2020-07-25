483fe5870a05cd30640d4de96e0385a0c72effc4
wadexi <wadexi2207@163.com>
2020-07-25 21:09:16 +0800

    feign consumer config

--- feign-consumer/src/main/java/com/study/feignconsumer/controller/ConsumerController.java
+++ feign-consumer/src/main/java/com/study/feignconsumer/controller/ConsumerController.java
@@ -1,25 +1,31 @@
 package com.study.feignconsumer.controller;
 
 import com.study.feignconsumer.service.HelloService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class ConsumerController {
 
 
     @Autowired
-    private HelloService helloService3;
+    private HelloService helloService;
 
 //    @Autowired
 //    private HelloService3 helloService32;
 
+    @Value("${testnumber}")
+    private int testNum;
 
     @RequestMapping(value = "/feign-consumer",method = RequestMethod.GET)
     public String helloConsumer(){
-        return helloService3.hello();
+        log.error("testNum:" + testNum);
+        return helloService.hello();
     }
 
 
