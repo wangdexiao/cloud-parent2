ce405665d40e2a673cae90b0d3793515c9466b72
wadexi <wadexi2207@163.com>
2020-07-25 19:31:19 +0800

    配置中心server

---
+++ config-server/src/main/java/com/study/configserver/ConfigServerApplication.java
@@ -0,0 +1,20 @@
+package com.study.configserver;
+
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(ConfigServerApplication.class)
+                .web(WebApplicationType.SERVLET)
+                .run(args);
+    }
+
+}
