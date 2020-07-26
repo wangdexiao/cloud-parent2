70e0ae569d1e9994f3276a3d102c104bb655c85a
wadexi <wadexi2207@163.com>
2020-07-26 16:56:37 +0800

    config-server 添加cloud bus

--- config-server/src/main/java/com/study/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/com/study/configserver/ConfigServerApplication.java
@@ -4,16 +4,18 @@ import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableEurekaClient
 @EnableConfigServer
 @SpringBootApplication
+@RefreshScope
 public class ConfigServerApplication {
 
     public static void main(String[] args) {
         new SpringApplicationBuilder(ConfigServerApplication.class)
-                .web(WebApplicationType.SERVLET)
+//                .web(WebApplicationType.SERVLET)
                 .run(args);
     }
 
