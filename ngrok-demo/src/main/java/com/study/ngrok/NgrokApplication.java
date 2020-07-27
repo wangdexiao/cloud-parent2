150912ab989407794e2f800400ca7cb9a8400796
wadexi <wadexi2207@163.com>
2020-07-27 22:35:56 +0800

    ngrok 访问demo

---
+++ ngrok-demo/src/main/java/com/study/ngrok/NgrokApplication.java
@@ -0,0 +1,12 @@
+package com.study.ngrok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NgrokApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NgrokApplication.class, args);
+    }
+}
---
+++ ngrok-demo/src/main/java/com/study/ngrok/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.study.ngrok.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @RequestMapping(method = RequestMethod.GET,path = "/test")
+    public String test(){
+        return "test";
+    }
+}
